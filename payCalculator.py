3288e51abdcd60162910c31159604dacf13891de
Larry Winton <Larry.Winton@vu.com>
2022-01-17 11:39:04 -0600

    retry (1) failed test

--- payCalculator.py
+++ payCalculator.py
@@ -8,11 +8,10 @@ def calculatePay():
     fRate = float(sRate)
     pay = fHours * fRate
     print(pay)
-    
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+#calculatePay()
