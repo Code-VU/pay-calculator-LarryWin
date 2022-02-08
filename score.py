9b15ef5648309061d2c9ab87ff86e7aee88d4a0d
Larry Winton <Larry.Winton@vu.com>
2022-02-08 11:39:37 -0600

    assignment 3.3 try 0

---
+++ score.py
@@ -0,0 +1,17 @@
+def score():
+    sScore = input("Enter score: ")
+    fScore = float(sScore)
+    if (fScore > 1.0 or fScore < 0.0):
+        print('Bad score')
+    elif (fScore >= 0.9):
+        print('A')
+    elif ( (fScore >= 0.8) and (fScore < 0.9) ):
+        print('B')
+    elif ( (fScore >= 0.7) and (fScore < 0.8) ):
+        print('C')
+    elif ( (fScore >= 0.6) and (fScore < 0.7) ):
+        print('D')
+    else:
+        print('F')
+
+#score()
