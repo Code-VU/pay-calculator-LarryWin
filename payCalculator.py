c1ace62308ab6fd24811bf0250fecee5b0570427
Larry Winton <Larry.Winton@vu.com>
2022-02-08 11:55:15 -0600

    Optional assignment 3.2

--- payCalculator.py
+++ payCalculator.py
@@ -2,28 +2,26 @@ def calculatePay():
     # Implement your solution in between the two comment blocks
     print("calculating pay")
     # This first line is provided for you
-    sHours = input("Enter Hours: ")
-    sRate = input("Enter Rate: ")
-    fHours = float(sHours)
-    fRate = float(sRate)
-    fBaseHours = 40.0
-    fOvertimeHours = fHours - fBaseHours
-    fOvertimePay = 0
-    fBasePay = 0
-    if (fOvertimeHours > 0):
-        fOvertimePay = fOvertimeHours * 1.5 * fRate
-    if (fHours > 40):
-        fBasePay = 40 * fRate
-    else:
-        fBasePay = fHours * fRate
-    
-    pay = fBasePay + fOvertimePay
-    print(round(pay, 1))
-
-    # x = int(98.6)
-    # print("type: ", type(x))
-    # print(x)
-    # end assignment
+    try:
+        sHours = input("Enter Hours: ")
+        fHours = float(sHours)
+        sRate = input("Enter Rate: ")
+        fRate = float(sRate)
+        fBaseHours = 40.0
+        fOvertimeHours = fHours - fBaseHours
+        fOvertimePay = 0
+        fBasePay = 0
+        if (fOvertimeHours > 0):
+            fOvertimePay = fOvertimeHours * 1.5 * fRate
+        if (fHours > 40):
+            fBasePay = 40 * fRate
+        else:
+            fBasePay = fHours * fRate
+        
+        pay = fBasePay + fOvertimePay
+        print(round(pay, 1))
+    except:
+        print('Error, please enter numeric input')
 
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
