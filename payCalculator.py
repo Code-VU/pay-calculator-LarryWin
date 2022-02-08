238cf77eae6626867ba3883e29aec131a06eb975
Larry Winton <Larry.Winton@vu.com>
2022-02-08 12:42:17 -0600

    assignment 4.6

--- payCalculator.py
+++ payCalculator.py
@@ -1,22 +1,28 @@
 def calculatePay():
-    # Implement your solution in between the two comment blocks
-    print("calculating pay")
-    # This first line is provided for you
     try:
         sHours = input("Enter Hours: ")
         fHours = float(sHours)
         sRate = input("Enter Rate: ")
         fRate = float(sRate)
+        compute_pay(fHours, fRate)
+    except:
+        print('Error, please enter numeric input')
+
+def compute_pay(hours, rate):
+    # Implement your solution in between the two comment blocks
+    # print("calculating pay")
+    # This first line is provided for you
+    try:
         fBaseHours = 40.0
-        fOvertimeHours = fHours - fBaseHours
+        fOvertimeHours = hours - fBaseHours
         fOvertimePay = 0
         fBasePay = 0
         if (fOvertimeHours > 0):
-            fOvertimePay = fOvertimeHours * 1.5 * fRate
-        if (fHours > 40):
-            fBasePay = 40 * fRate
+            fOvertimePay = fOvertimeHours * 1.5 * rate
+        if (hours > 40):
+            fBasePay = 40 * rate
         else:
-            fBasePay = fHours * fRate
+            fBasePay = hours * rate
         
         pay = fBasePay + fOvertimePay
         print(round(pay, 1))
@@ -27,4 +33,4 @@ def calculatePay():
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-#calculatePay()
+calculatePay()
